bdfb085cfe5308891baeff268e4cd4c1ec743ddd
Sergey <ser3421@gmail.com>
2019-08-19 18:30:18 +0400

    fourhomework

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -6,12 +6,15 @@ from .models import Article, Scope
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-    context['object_list'] = Article.objects.all()
-    # context['tags'] = []
-    # for article in Article.objects.all():
-    #     tag = article.scope_set.all().order_by('-is_main', 'tematik')
-    #     context['tags'].append(tag)
+    num = {}
+    for article in Article.objects.all():
+        tag = article.scope_set.all().order_by('-is_main', 'tematik')
+        num[article.id] = tag
+
+    context = {
+        'object_list': Article.objects.all(),
+        'tass': num
+    }
 
     return render(request, template, context)
 
