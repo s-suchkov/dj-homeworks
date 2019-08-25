e709e410a1a19e33fbf5556a6b63d5d1d41f0987
Sergey <ser3421@gmail.com>
2019-08-25 22:55:03 +0700

    calc

--- site-form-works/credit_calc/app/views.py
+++ site-form-works/credit_calc/app/views.py
@@ -6,9 +6,27 @@ from .forms import CalcForm
 def calc_view(request):
     template = "app/calc.html"
 
-    form = CalcForm
-    context = {
-        'form': form
-    }
 
+    if request.method == 'POST':
+        form = CalcForm(request.POST)
+        if form.is_valid():
+            initial_fee = form.cleaned_data['initial_fee']
+            rate = form.cleaned_data['rate']
+            months_count = form.cleaned_data['months_count']
+            common_result = initial_fee + initial_fee * rate / 100
+            result = round(common_result / months_count, 2)
+            print(form)
+            context = {
+                'form': form,
+                'common_result': common_result,
+                'result': result
+            }
+    else:
+        form = CalcForm()
+        context = {
+            'form': form
+        }
     return render(request, template, context)
+
+# form = CalcForm.objects.all()
+# print(form)
