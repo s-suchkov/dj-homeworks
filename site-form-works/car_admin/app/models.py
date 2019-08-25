e32f503b4a9471e245421133e929929ee7e4dff1
Sergey Suchkov <41924369+s-suchkov@users.noreply.github.com>
2019-08-25 21:33:22 +0700

    avia

--- site-form-works/car_admin/app/models.py
+++ site-form-works/car_admin/app/models.py
@@ -5,6 +5,10 @@ class Car(models.Model):
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = ("Машина")
+        verbose_name_plural = ("Машины")
+
     def __str__(self):
         return f'{self.brand} {self.model}'
 
@@ -16,6 +20,11 @@ class Review(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField()
+
+    class Meta:
+        verbose_name = ("Статья")
+        verbose_name_plural = ("Статьи")
+
 
     def __str__(self):
         return str(self.car) + ' ' + self.title
