3f7ccabda981099c57c32a8c9fbe4161fade5099
Sergey Suchkov <41924369+s-suchkov@users.noreply.github.com>
2019-08-25 21:18:09 +0700

    avia

--- site-form-works/avia_scanner/app/forms.py
+++ site-form-works/avia_scanner/app/forms.py
@@ -1,9 +1,23 @@
 from django import forms
-
-from .widgets import AjaxInputWidget
 from .models import City
-
-
+from .widgets import AjaxInputWidget
+# from app.views import cache_choices
 class SearchTicket(forms.Form):
+    departure = forms.CharField(
+        label='Вылет',
+        widget=AjaxInputWidget(
+            url='api/city_ajax',
+            attrs={'class': 'inline right-margin'}
+        )
+    )
+    arrival = forms.ChoiceField(
+        label='Прилет',
+        choices=[(x, x) for x in City.objects.all()]
+    )
+    date = forms.DateField(
+        label='Дата',
+        # widget=forms.DateInput(attrs={'type':'date'})
+        widget=forms.SelectDateWidget
+    )
     # Добавьте здесь поля, описанные в задании
     pass
--- site-form-works/avia_scanner/app/views.py
+++ site-form-works/avia_scanner/app/views.py
@@ -1,6 +1,6 @@
 import time
 import random
-
+import re
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.core.cache import cache
@@ -21,5 +21,27 @@ def ticket_page_view(request):
 
 def cities_lookup(request):
     """Ajax request предлагающий города для автоподстановки, возвращает JSON"""
+    search = request.GET
+    term = search['term']
     results = []
+    cities = cache.get('key')
+    if cities is None:
+        value = City.objects.all()
+        cache.set('key', value)
+    for city in cities:
+        if re.match(term.lower(), city.name.lower()):
+            results.append(city.name)
     return JsonResponse(results, safe=False)
+
+
+
+# def cache_choices():
+#     value = []
+#     if cache.get('key') is None:
+#         for city in City.objects.all():
+#             cache.set('key', city.name)
+#             value.append(cache.get('key'))
+#             print(cache.get('key'))
+#     return value
+#
+# print(cache_choices())
