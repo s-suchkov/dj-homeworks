6e264869fa0e9c5bf248a277add0f8ca7133d557
Sergey <ser3421@gmail.com>
2019-08-25 22:58:16 +0700

    rewiew

--- site-form-works/review/app/forms.py
+++ site-form-works/review/app/forms.py
@@ -5,6 +5,14 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea, label='Отзыв')
 
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if text == None:
+            raise forms.ValidationError("Нельзя отправить пустое поле")
+        return text
+
     class Meta(object):
         model = Review
         exclude = ('id', 'product')
+
+
--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.urls import reverse
-
+from django.views import View
 from .models import Product, Review
 from .forms import ReviewForm
-
+from django.http import HttpResponse
 
 def product_list_view(request):
     template = 'app/product_list.html'
@@ -16,18 +16,58 @@ def product_list_view(request):
     return render(request, template, context)
 
 
-def product_view(request, pk):
+# def product_view(request, pk):
+#     template = 'app/product_detail.html'
+#     product = get_object_or_404(Product, id=pk)
+#
+#     form = ReviewForm
+#     if request.method == 'POST':
+#         # логика для добавления отзыва
+#         pass
+#
+#     context = {
+#         'form': form,
+#         'product': product
+#     }
+#
+#     return render(request, template, context)
+
+class MyForm(View):
+    form_class = ReviewForm
     template = 'app/product_detail.html'
-    product = get_object_or_404(Product, id=pk)
 
-    form = ReviewForm
-    if request.method == 'POST':
-        # логика для добавления отзыва
-        pass
+    def get(self, request, pk):
+        product = get_object_or_404(Product, id=pk)
 
-    context = {
-        'form': form,
-        'product': product
-    }
+        context = {
+            'form': self.form_class,
+            'product': product,
+            'reviews': product.review_set.all(),
+        }
+        return render(request, self.template, context)
 
-    return render(request, template, context)
+    def post(self, request, pk):
+
+        form = self.form_class(request.POST)
+        product = get_object_or_404(Product, id=pk)
+        id = []
+        request.session['id'] = id
+
+        if form.is_valid() and not(pk in request.session['id']):
+            print(not(pk in request.session['id']))
+            reviews = product.review_set.create(text=form.cleaned_data['text'])
+            request.session['text'] = form.cleaned_data['text']
+            id.append(pk)
+            request.session['s_review_exist'] = True
+            context = {
+            'reviews': product.review_set.all(),
+            'product': product,
+            'is_review_exist': request.session['s_review_exist']
+            }
+        else:
+            context = {
+                'reviews': product.review_set.all(),
+                'product': product,
+                'is_review_exist': request.session['s_review_exist']
+            }
+        return render(request, self.template, context)
