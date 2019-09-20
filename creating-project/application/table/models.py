ef607951b84b9f79d2a6cf8cff7b8d4e83acc0ef
Sergey <ser3421@gmail.com>
2019-09-20 19:04:49 +0300

    all

--- creating-project/application/table/models.py
+++ creating-project/application/table/models.py
@@ -2,11 +2,11 @@ from django.db import models
 import csv
 class Fields(models.Model):
     name = models.TextField()
-    width = models.FloatField()
+    width = models.IntegerField()
     id = models.IntegerField(primary_key=True)
 
 class Path_csv(models.Model):
-    file = models.FileField(upload_to='application/phones.csv')
+    file = models.FileField()
     @staticmethod
     def get_path(file):
         with open(file) as f:
--- creating-project/application/table/views.py
+++ creating-project/application/table/views.py
@@ -1,21 +1,24 @@
 import csv
-
+from .models import Path_csv, Fields
 from django.shortcuts import render
-
-
-CSV_FILENAME = 'phones.csv'
-COLUMNS = [
-    {'name': 'id', 'width': 1},
-    {'name': 'name', 'width': 3},
-    {'name': 'price', 'width': 2},
-    {'name': 'release_date', 'width': 2},
-    {'name': 'lte_exists', 'width': 1},
-]
+import os
+if len(Path_csv.objects.all())==0:
+    Path_csv.objects.create(file=os.path.abspath("phones.csv"))
+# CSV_FILENAME = 'phones.csv'
+# COLUMNS = [
+#     {'name': 'id', 'width': 1},
+#     {'name': 'name', 'width': 3},
+#     {'name': 'price', 'width': 2},
+#     {'name': 'release_date', 'width': 2},
+#     {'name': 'lte_exists', 'width': 1},
+# ]
 
 
 def table_view(request):
+    CSV_FILENAME = Path_csv.objects.all()[0].file
+    COLUMNS = Fields.objects.all().order_by('id')
     template = 'table.html'
-    with open(CSV_FILENAME, 'rt') as csv_file:
+    with open(f'{CSV_FILENAME}', 'rt') as csv_file:
         header = []
         table = []
         table_reader = csv.reader(csv_file, delimiter=';')
